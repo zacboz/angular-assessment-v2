995ea762307b9c7d64c6bc6316e99f5a9305b983
zacboz <zacboz@gmail.com>
2017-01-19 18:23:36 -0700

    finished angular assessment 2

---
+++ app.js
@@ -0,0 +1,30 @@
+angular.module('assessment', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+      $stateProvider
+            .state('home',{
+              url:'/',
+              templateUrl: './views/home.html'
+            })
+            .state('pets',{
+                url:'/pets',
+                templateUrl: './views/pets.html',
+                controller: 'mainCtrl'
+            })
+            .state('blog',{
+                url:'/blog',
+                templateUrl: './views/blog.html'
+            })
+            .state('about',{
+                url:'/about',
+                templateUrl: './views/about.html'
+            })
+            .state('bio',{
+                url:'/bio/:id',
+                templateUrl: './views/pet-bio.html',
+                controller: 'mainCtrl'
+            });
+
+            $urlRouterProvider
+                .otherwise('/');
+
+  });
---
+++ mainService.js
@@ -0,0 +1,24 @@
+angular.module('assessment').service('mainService', function($http){
+
+  this.getPets = function() {
+    return $http({
+      method: 'GET',
+      url: 'http://practiceapi.devmounta.in/pets'
+    }).then(function(response){
+      // console.log(response);
+      return response;
+    });
+  };
+
+  this.getBio = function(id) {
+    console.log(id);
+    return $http({
+      method: 'GET',
+      url: 'http://practiceapi.devmounta.in/pets/'+id
+    }).then(function(response){
+      console.log(response);
+      return response;
+    });
+  };
+
+});
